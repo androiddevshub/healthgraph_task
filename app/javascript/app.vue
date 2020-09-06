d8910a00c08522dda22814d01ab0136aed0ed2ee
Shubham jain <shubjain440@gmail.com>
2020-09-06 12:14:27 +0530

    Load vuejs and added navbar

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <div>
+      <b-navbar toggleable="lg" variant="info">
+        <b-navbar-brand href="#">Homes</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+             <b-nav-form>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">New Post</b-button>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Trash</b-button>
+            </b-nav-form>
+            
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
   </div>
 </template>
 
